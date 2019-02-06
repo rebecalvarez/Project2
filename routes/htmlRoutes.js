ae6152af093e74663ea9b59b9adbd601e1591da1
ianbunn <ianrichardbunn@gmail.com>
2019-02-05 20:12:44 -0600

    adding user get route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -13,6 +13,10 @@ module.exports = function(app) {
     // });
   });
 
+  app.get("/user", function(req, res) {
+    res.render("user");
+  });
+
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(
