553037fd894c88f68305e29ad7c51aeba5eb4d75
ianbunn <ianrichardbunn@gmail.com>
2019-02-05 19:12:32 -0600

    linted stockfaves,apiroutes,htmlroutes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,5 +1,6 @@
 var db = require("../models");
-var User=require("../models/user");
+// var User = require("../models/user");
+
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
@@ -14,7 +15,9 @@ module.exports = function(app) {
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
       res.render("example", {
         example: dbExample
       });
