282968c445050113d600308e54c3bfc6db8c215a
rebecalvarez <rebeca.dodero@gmail.com>
2019-02-05 18:50:04 -0600

    front end update

---
+++ public/php/send.php
@@ -0,0 +1,26 @@
+<?php
+if(!$_POST) exit;
+
+    $to 	  = 'someemail@somedomain.com'; #Replace your email id...
+	$name	  = $_POST['txtname'];
+	$email    = $_POST['txtemail'];
+    $comment  = $_POST['txtmessage'];
+        
+	if(get_magic_quotes_gpc()) { $comment = stripslashes($comment); }
+
+	 $e_subject = 'You\'ve been contacted by ' . $name . '.';
+
+	 $msg  = "You have been contacted by $name with regards to, support.\r\n\n";
+	 $msg .= "$comment\r\n\n";
+	 $msg .= "You can contact $name via email, $email.\r\n\n";
+	 $msg .= "-------------------------------------------------------------------------------------------\r\n";
+								
+	 if(@mail($to, $e_subject, $msg, "From: $email\r\nReturn-Path: $email\r\n"))
+	 {
+		 echo "<span class='success-msg'>Thanks for Contacting Us, We will call back to you soon.</span>";
+	 }
+	 else
+	 {
+		 echo "<span class='error-msg'>Sorry your message not sent, Try again Later.</span>";
+	 }
+?>
